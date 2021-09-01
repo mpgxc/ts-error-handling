74f2ec64ee0b6c41052ef7a5279093a764b4ae7a
mpgxc <mpgx5.c@gmail.com>
2021-09-01 04:22:43 -0300

    chore: testing flow call methods and errors  handlers

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { AppException } from './application/AppException';
 import { Account } from './domain/Account';
 import { AccountMapper } from './domain/AccountMapper';
 import { Email } from './domain/Email';
@@ -6,12 +7,17 @@ import { Password } from './domain/Password';
 
 const name = Name.build('Um bom name');
 const email = Email.build('sdsdsWWFSFSdsdsd@gmail.com  ');
-const password = Password.build('und#eFfi2ned');
+
+const passwordOrError = Password.build('un2ned');
+
+if (passwordOrError.isFailure) {
+    throw AppException.build(passwordOrError.value, 400);
+}
 
 const account = Account.build({
     email,
     name,
-    password,
+    password: passwordOrError.value,
 });
 
 console.log(AccountMapper.toPersistence(account));
