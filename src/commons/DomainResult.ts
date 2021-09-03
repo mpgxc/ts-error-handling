ac3e72a684f08f73b9e929b41e449fadaf8c7f7c
mpgxc <mpgx5.c@gmail.com>
2021-09-03 03:42:11 -0300

    refactor: domain custom class error

--- src/commons/DomainResult.ts
+++ src/commons/DomainResult.ts
@@ -1,32 +1,26 @@
 interface IDomainResult<S, F> {
     value: S | F;
-
     isSuccess: boolean;
-    isFailure: boolean;
 }
 
 interface ISuccessResult<S, F> extends IDomainResult<S, F> {
     value: S;
     isSuccess: true;
-    isFailure: false;
 }
 
 interface IFailureResult<S, F> extends IDomainResult<S, F> {
     value: F;
     isSuccess: false;
-    isFailure: true;
 }
 
 const Success = <S, F>(value: S): ISuccessResult<S, never> => ({
     value,
     isSuccess: true,
-    isFailure: false,
 });
 
 const Failure = <S, F>(value: F): IFailureResult<never, F> => ({
     value,
     isSuccess: false,
-    isFailure: true,
 });
 
 type Either<S, F> = ISuccessResult<S, F> | IFailureResult<S, F>;
