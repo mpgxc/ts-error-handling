65f19aedb2063b796bc109c32fd112dd271fc2f6
mpgxc <mpgx5.c@gmail.com>
2021-09-30 01:53:06 -0300

    refactor: change class instance to simple object

--- src/domain/Errors.ts
+++ src/domain/Errors.ts
@@ -1,22 +1,22 @@
-import { DomainError } from '../commons/DomainError';
+type IDomainErrorProps = {
+    message: string;
+    name: string;
+};
 
-const InvalidPassword = (value: string): DomainError =>
-    new DomainError({
-        message: `The ${value} must contain between 8 to 32 digits and at least 1 number,
+const InvalidPassword = (value: string): IDomainErrorProps => ({
+    message: `The ${value} must contain between 8 to 32 digits and at least 1 number,
         1 special character, 1 uppercase letter and 1 lowercase letter`,
-        name: 'PasswordError',
-    });
+    name: 'PasswordError',
+});
 
-const InvalidEmail = (value: string): DomainError =>
-    new DomainError({
-        message: `The ${value} is wrong, be sure to enter a valid email address! `,
-        name: 'EmailError',
-    });
+const InvalidEmail = (value: string): IDomainErrorProps => ({
+    message: `The ${value} is wrong, be sure to enter a valid email address! `,
+    name: 'EmailError',
+});
 
-const InvalidName = (value: string): DomainError =>
-    new DomainError({
-        message: `The ${value} must contain between 5 to 255 caracters `,
-        name: 'NameError',
-    });
+const InvalidName = (value: string): IDomainErrorProps => ({
+    message: `The ${value} must contain between 5 to 255 caracters `,
+    name: 'NameError',
+});
 
 export { InvalidPassword, InvalidEmail, InvalidName };
